f2d5fae0891521e6be889704d2ac19e14955bb32
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-02-13 17:19:02 +0000

    Do not use system boost - breaks YCM - not sure why - to debug

--- bin/updateYCM.sh
+++ bin/updateYCM.sh
@@ -2,5 +2,7 @@ echo "Rebuilding YCM..."
 
 pushd ~/.vim/plugged/YouCompleteMe
     find . -name "*pyc" -exec rm -fv '{}' \;
-    python3 ./install.py --clang-completer --system-libclang --system-boost
+    # using system boost is breaking it somehow
+    # python3 ./install.py --clang-completer --system-libclang --system-boost
+    python3 ./install.py --clang-completer --system-libclang
 popd
