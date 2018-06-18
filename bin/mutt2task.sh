d0c560c088423bc3d874769509a5959eac7c8dcd
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-18 14:28:10 +0100

    Also add sender information in my tasks

--- bin/mutt2task.sh
+++ bin/mutt2task.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 # From https://www.nixternal.com/mark-e-mails-in-mutt-as-tasks-in-taskwarrior/
-task add project:email due:1d E-mail: "$(sed -n '/^Subject: /{s///p;q}')"
+sender="$(sed -n '/^From: /{s///p;q}')"
+subject="$(sed -n '/^Subject: /{s///p;q}')"
+task add project:email due:1d "$sender: $subject"
