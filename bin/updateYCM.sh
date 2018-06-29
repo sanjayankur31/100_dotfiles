9462cd9ef4fd971b2814cc1c37388e4de39119d6
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-06-29 19:59:58 +0100

    Also build javascript bits

--- bin/updateYCM.sh
+++ bin/updateYCM.sh
@@ -4,5 +4,5 @@ pushd ~/.vim/plugged/YouCompleteMe
     find . -name "*pyc" -exec rm -fv '{}' \;
     # using system boost is breaking it somehow
     # python3 ./install.py --clang-completer --system-libclang --system-boost
-    python3 ./install.py --clang-completer --system-libclang
+    python3 ./install.py --clang-completer --system-libclang --js-completer --java-completer
 popd
