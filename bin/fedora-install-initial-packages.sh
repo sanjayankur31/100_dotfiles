c08ae7c88c4ddfb5c42ec3b61dca51786947ab99
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2019-04-24 23:48:46 +0100

    Update script to use getopts

--- bin/fedora-install-initial-packages.sh
+++ bin/fedora-install-initial-packages.sh
@@ -1,52 +1,130 @@
 #!/bin/bash
-# Initial package installation
-
 REL="$(rpm -E %fedora)"
-echo "Installing basic packages for Fedora $REL."
+echo "We are running Fedora $REL."
 
-sudo dnf update --refresh
+function setup_repos() {
+    # Neomutt
+    sudo dnf copr enable flatcap/neomutt
 
-# Neomutt
-sudo dnf copr enable flatcap/neomutt
+    # RPMFusion and adobe
+    sudo dnf install \
+        https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$REL".noarch.rpm \
+        https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$REL".noarch.rpm \
+        http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 
-# RPMFusion and adobe
-sudo dnf install \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$REL".noarch.rpm \
-    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$REL".noarch.rpm \
-    http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+    sudo dnf update --refresh
+}
 
-sudo dnf group upgrade --with-optional Multimedia
 
-# Music and multimedia
-# https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
-# https://rpmfusion.org/Configuration
+function update_groups() {
+    sudo dnf group upgrade --with-optional Multimedia
 
-sudo dnf groupupdate multimedia
-sudo dnf groupupdate sound-and-video
+    # Music and multimedia
+    # https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
+    # https://rpmfusion.org/Configuration
+    sudo dnf groupupdate multimedia
+    sudo dnf groupupdate sound-and-video
 
-# Fusion appstream data
-sudo dnf groupupdate core
+    # Fusion appstream data
+    sudo dnf groupupdate core
 
-# Basics
-sudo dnf install byobu tmux htop syncthing vit task neomutt weechat ncmpcpp \
-    mpv vimiv vifm fedora-packager git-all offlineimap msmtp notmuch gnuplot \
-    /usr/bin/ag aria2 qutebrowser cscope ctags fedora-review mpd vim-enhanced \
-    vim-X11 notmuch-vim notmuch-mutt rcm pwgen pass \
-    python3-websocket-client xsel flash-plugin deja-dup parcellite \
-    anka-coder-fonts-* zathura zathura-plugins-all urlscan timew \
-    gnome-pomodoro docker podman --setopt=strict=0
+}
 
-# texlive bits
-sudo dnf install texlive /usr/bin/pdflatex /usr/bin/latexmk /usr/bin/chktex \
-    /usr/bin/lacheck /usr/bin/biber /usr/bin/flake8-3 /usr/bin/perlcritic \
-    /usr/bin/rstcheck texlive-epstopdf texlive-biblatex-nature \
-    texlive-beamertheme-metropolis pdfpc --setopt=strict=0
+function install_basics() {
+    # Basics
+    sudo dnf install byobu tmux htop syncthing vit task neomutt weechat ncmpcpp \
+        mpv vimiv vifm fedora-packager git-all offlineimap msmtp notmuch gnuplot \
+        /usr/bin/ag aria2 qutebrowser cscope ctags fedora-review mpd vim-enhanced \
+        vim-X11 notmuch-vim notmuch-mutt rcm pwgen pass \
+        python3-websocket-client xsel flash-plugin deja-dup parcellite \
+        anka-coder-fonts-* zathura zathura-plugins-all urlscan timew \
+        gnome-pomodoro docker podman --setopt=strict=0
+}
 
-# Flatpaks
-echo "Installing flatpaks from Flathub"
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+function install_texlive_packages() {
+    # texlive bits
+    sudo dnf install texlive /usr/bin/pdflatex /usr/bin/latexmk /usr/bin/chktex \
+        /usr/bin/lacheck /usr/bin/biber /usr/bin/flake8-3 /usr/bin/perlcritic \
+        /usr/bin/rstcheck texlive-epstopdf texlive-biblatex-nature \
+        texlive-beamertheme-metropolis pdfpc --setopt=strict=0
+}
 
-flatpak install flathub com.spotify.Client
-flatpak install flathub com.uploadedlobster.peek
-flatpak install flathub org.telegram.desktop
-flatpak install flathub org.signal.Signal
+
+function install_flatpaks() {
+    # Flatpaks
+    echo "Installing flatpaks from Flathub"
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+    flatpak install flathub com.spotify.Client
+    flatpak install flathub com.uploadedlobster.peek
+    flatpak install flathub org.telegram.desktop
+    flatpak install flathub org.signal.Signal
+}
+
+
+function usage() {
+    echo "$0: Install required Texlive packages for a LaTeX project on Fedora"
+    echo
+    echo "Usage: $0 [-sitfa]"
+    echo
+    echo "-s: set up DNF repos"
+    echo "-u: update groups: implies -s"
+    echo "-b: install basics: implies -s"
+    echo "-t: install TeXlive packages: implies -s"
+    echo "-f: install flatpaks from flathub: also sets up flathub"
+    echo "-a: do all of the above"
+    echo "-h: print this usage text and exit"
+}
+
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+# parse options
+while getopts "sbtfah" OPTION
+do
+    case $OPTION in
+        s)
+            setup_repos
+            exit 0
+            ;;
+        u)
+            setup_repos
+            update_groups
+            exit 0
+            ;;
+        b)
+            setup_repos
+            install_basics
+            exit 0
+            ;;
+        t)
+            setup_repos
+            install_texlive_packages
+            exit 0
+            ;;
+        f)
+            setup_repos
+            install_flatpaks
+            exit 0
+            ;;
+        a)
+            setup_repos
+            update_groups
+            install_basics
+            install_texlive_packages
+            install_flatpaks
+            exit 1
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
