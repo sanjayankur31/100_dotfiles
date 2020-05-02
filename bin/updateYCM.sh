133bfdeebfea838655aa2708abc42fea5caed6b1
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-05-02 19:31:08 +0100

    Remove tern for YCM

--- bin/updateYCM.sh
+++ bin/updateYCM.sh
@@ -5,5 +5,5 @@ pushd ~/.vim/plugged/YouCompleteMe
     # using system boost is breaking it somehow
     # python3 ./install.py --clang-completer --system-libclang --system-boost
     python3 ./install.py --clang-completer --system-libclang \
-        --js-completer --java-completer --ts-completer
+        --java-completer --ts-completer
 popd
