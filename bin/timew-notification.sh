cd3515d44cb8b1687225edbb5618dcabd9212107
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-09-24 15:21:14 +0100

    Add notification script for timew

---
+++ bin/timew-notification.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright 2020 Ankur Sinha
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# File : timew-notification.sh
+#
+# Send a notification about the current timew status
+# To be maily used as a cronjob
+
+if [ -x "/usr/bin/notify-send" ]
+then
+    notify-send -u low -t 2000 -i gnome-pomodoro "Timew status:" "$(timew)"
+fi
