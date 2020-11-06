a5d92ae36703a947bdf5dd809ac104becfe1fe1c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-11-06 09:44:56 +0000

    Add script to set context for taskwarrior

---
+++ bin/set-task-contexts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright 2020 Ankur Sinha
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+# File : set-task-contexts.sh
+#
+
+# Set contexts for task at different times
+# Needs to be run once at the start of the day
+
+echo "task context work" | at 0930
+echo "task context volunteering" | at 1700
+echo "task context none"  | at 1800
