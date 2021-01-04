d4c9afe64e94655863927ccb9e3bf5b69be83017
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-01-04 22:08:31 +0000

    Use wrapper to allow selecting mailbox before composing to correctly set mail options in neomutt

---
+++ bin/neomutt-compose-wrapper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright 2021 Ankur Sinha
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# File : neomutt-compose-wrapper.sh
+#
+# Wrapper around neomutt to let me pick a mailbox when clicking on neomutt
+# links
+
+NEOMUTT_CONFDIR="~/Sync/99_private/neomuttdir/"
+echo "Mailbox to use?"
+echo "1: gmail"
+echo "2: ucl"
+echo "3: hotmail"
+echo "4: yahoo"
+echo "5: herts"
+echo "6: herts-student"
+echo -n "> "
+read MAILBOX_ID
+
+case $MAILBOX_ID in
+    1)
+        MAILBOX="gmail"
+        ;;
+    2)
+        MAILBOX="ucl"
+        ;;
+    3)
+        MAILBOX="hotmail"
+        ;;
+    4)
+        MAILBOX="yahoo"
+        ;;
+    5)
+        MAILBOX="herts"
+        ;;
+    6)
+        MAILBOX="herts-student"
+        ;;
+esac
+# Change mailbox before composing
+echo "source $NEOMUTT_CONFDIR/$MAILBOX.neomuttrc"
+neomutt -e "source $NEOMUTT_CONFDIR/$MAILBOX.neomuttrc" "$@"
