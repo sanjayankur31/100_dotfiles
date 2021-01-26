0bf385822cdcd058e60102fb31d49d0275e85735
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-01-26 13:44:36 +0000

    Correct timestamp in backup script

--- bin/backup-qutebrowser.sh
+++ bin/backup-qutebrowser.sh
@@ -7,7 +7,7 @@
 QUTE_DATADIR="$HOME/.local/share/qutebrowser/sessions/"
 QUTE_CONFDIR="$HOME/.config/qutebrowser/"
 QUTE_BACKUPDIR="$HOME/Sync/qutebrowser-backup/$HOSTNAME/"
-timestamp="$(date +%Y%m%H%M)"
+timestamp="$(date +%Y%m%d%H%M)"
 
 mkdir -p "$QUTE_BACKUPDIR" || exit -1
 
