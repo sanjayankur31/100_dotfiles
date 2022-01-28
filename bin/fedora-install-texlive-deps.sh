c8bb089701bd7535df4fc56d70621de521e16344
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-01-28 20:22:36 +0000

    fix: correct naive parsing logic in LaTeX installer

--- bin/fedora-install-texlive-deps.sh
+++ bin/fedora-install-texlive-deps.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Copyright 2019 Ankur Sinha
+# Copyright 2021 Ankur Sinha
 # Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 # File :  fedora-install-texlive-deps.sh
 # Install Texlive dependencies for a LaTeX project on Fedora
+#
+# Note that this is a dumb parser, in the sense that it does not actually parse
+# the LaTeX text to "understand" it. It simply uses regular expressions to
+# extract patterns from the package commands. So, for example, it will *not*
+# ignore LaTeX comments.  So, it may report an over approximation of the
+# required packages.
+#
+# Uses commonly used Linux utilities: grep, sed
 
 REQUIREPACKAGES=""
 USEPACKAGES=""
 
 function fetchdeps() {
-    REQUIREPACKAGES=$(grep -rhiI "requirepackage" *.tex *.cls *.sty 2>/dev/null | sed -e "s|^.*\\\RequirePackage.*{|tex(|" -e "s|}.*$|\.sty)|" | sed -e "s|,|.sty) tex(|" | uniq -u | tr '\n' ' ')
+    REQUIREPACKAGES=$(grep -rhiI "requirepackage" *.tex *.cls *.sty 2>/dev/null | grep -oE "\{.*\}" | sed -e 's/,/}{/g' | sed -e "s/{/tex(/g" -e "s/}/.sty) /g" | tr '\n' ' ')
     REQUIREPACKAGES=${REQUIREPACKAGES}
-    USEPACKAGES=$(grep -rhiI "usepackage" *.tex *.cls *.sty 2>/dev/null | sed -e "s|^.*\\\usepackage.*{|tex(|" -e "s|}.*$|\.sty)|" | sed -e "s|,|.sty) tex(|" | uniq -u | tr '\n' ' ')
+    USEPACKAGES=$(grep -rhiI "usepackage" *.tex *.cls *.sty 2>/dev/null | grep -oE "\{.*\}" | sed -e 's/,/}{/g' | sed -e "s/{/tex(/g" -e "s/}/.sty) /g" | tr '\n' ' ')
     USEPACKAGES=${USEPACKAGES}
 
     echo "Following packages detected: $REQUIREPACKAGES $USEPACKAGES"
@@ -29,6 +37,7 @@ function usage() {
     echo
     echo "-d: dry run, only print required packages"
     echo "-i: also attempt to install packages"
+    echo "-h: print this help text and exit"
 }
 
 if [ $# -ne 1 ]
