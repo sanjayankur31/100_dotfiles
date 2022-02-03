830b9e224d87326ea97ed7edddf4314eb972ffcf
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-02-03 17:14:57 +0000

    feat: improve timew notification

--- bin/timew-notification.sh
+++ bin/timew-notification.sh
@@ -7,7 +7,9 @@
 # Send a notification about the current timew status
 # To be maily used as a cronjob
 
+TAGS=$(timew | grep -E 'Tracking' | sed -E 's/^.*Tracking[[:space:]]+//')
+TIME=$(timew | grep -E 'Total' | sed -E 's/^.*Total[[:space:]]+//')
 if [ -x "/usr/bin/notify-send" ]
 then
-    notify-send -t 1000 -i gnome-pomodoro -a  "Timew" "Timew" "$(timew > /dev/null 2>&1 && timew | grep -E 'Tracking|Total' | sed -e 's/Tracking //' -e 's/  Total/: /' )"
+    timew > /dev/null 2>&1 && notify-send -t 1000 -u normal -c im -i gnome-pomodoro -a  "Timew" "Timew" "${TIME}\n${TAGS}"
 fi
