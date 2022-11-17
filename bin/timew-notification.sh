3e2e3dad00f4d3d1372ae20a4eadad9b0cfe5e2c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-11-17 11:30:51 +0000

    feat(timew-notify): make gui notification an option

--- bin/timew-notification.sh
+++ bin/timew-notification.sh
@@ -4,12 +4,30 @@
 # Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
 # File : timew-notification.sh
 #
-# Send a notification about the current timew status
-# To be maily used as a cronjob
+#
+# Print one liner timew summary
+# Can also send a notification about the current timew status
+
+
+notification ()
+{
+    if [ -x "/usr/bin/notify-send" ]
+    then
+        timew > /dev/null 2>&1 && notify-send -t 1000 -u normal -c im -i gnome-pomodoro -a  "Timew" "Timew" "${TIME}\n${TAGS}"
+    fi
+}
 
 TAGS=$(timew | grep -E 'Tracking' | sed -E 's/^.*Tracking[[:space:]]+//')
 TIME=$(timew | grep -E 'Total' | sed -E 's/^.*Total[[:space:]]+//')
-if [ -x "/usr/bin/notify-send" ]
-then
-    timew > /dev/null 2>&1 && notify-send -t 1000 -u normal -c im -i gnome-pomodoro -a  "Timew" "Timew" "${TIME}\n${TAGS}"
-fi
+# Only print the first five letters of tag string: the idea is just to remind one of what the current task is
+echo "TW: ${TIME} ${TAGS:0:5}..."
+
+while getopts "n" OPTION
+do
+    case $OPTION in
+        n)
+            notification
+            exit 0
+            ;;
+    esac
+done
