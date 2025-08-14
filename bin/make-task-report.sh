7587364aeafc7bef4b4ef723fec0972aef19572d
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-08-14 23:30:19 +0100

    feat: allow providing location, capitalise for consistency

--- bin/make-task-report.sh
+++ bin/make-task-report.sh
@@ -24,11 +24,11 @@
 #!/bin/bash
 #
 
-OUTPUTDIR="${HOME}/Sync/taskreports/"
+OUTPUTDIRDEFAULT="${HOME}/Sync/taskreports/"
 #
 # if no date is provided, assume today/now
-enddatedefault=$(date +%Y-%m-%d -d "now")
-nowtime=$(date +%Y-%m-%dT%H:%M)
+ENDDATEDEFAULT=$(date +%Y-%m-%d -d "now")
+NOWTIME=$(date +%Y-%m-%dT%H:%M)
 
 mkdir -pv ${OUTPUTDIR}/
 
@@ -37,82 +37,88 @@ function taskreports() {
     # Update list as required
     for p in "foss" "job.ucl" "research" "personal"
     do
-        ${HOME}/bin/generate-taskreports.sh -p $p | ansi2html -w > ${OUTPUTDIR}/taskreport-$p-$enddate.html
+        ${HOME}/bin/generate-taskreports.sh -p $p | ansi2html -w > ${OUTPUTDIR}/taskreport-$p-$ENDDATE.html
     done
     # Generate combined report for everything
-    ${HOME}/bin/generate-taskreports.sh -a | ansi2html -w > ${OUTPUTDIR}/taskreport-all-$enddate.html
+    ${HOME}/bin/generate-taskreports.sh -a | ansi2html -w > ${OUTPUTDIR}/taskreport-all-$ENDDATE.html
 
 }
 
 function timesheets() {
-    echo "Generating time sheets ending ${enddate}"
-    week_start=$(date +%Y-%m-%d -d "${enddate} - 1 week")
-    month_start=$(date +%Y-%m-%d -d "${enddate} - 1 month")
+    echo "Generating time sheets ending ${ENDDATE}"
+    week_start=$(date +%Y-%m-%d -d "${ENDDATE} - 1 week")
+    month_start=$(date +%Y-%m-%d -d "${ENDDATE} - 1 month")
 
     # Time sheets
     # Can be split out into a different file perhaps
-    echo > ${OUTPUTDIR}/timesheet-$enddate.html
-    echo " -- Week: totals --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-    /usr/bin/timew report totals.py :week | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo > ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    echo " -- Week: totals --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    /usr/bin/timew report totals.py :week | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
 
-    echo >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
     for p in "foss" "job" "research" "personal" "volunteering" "career-development"
     do
-        echo " -- Week: $p --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-        /usr/bin/timew summary "${week_start}" - "${endtimestamp}" "$p" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+        echo " -- Week: $p --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+        /usr/bin/timew summary "${week_start}" - "${ENDTIMESTAMP}" "$p" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
     done
 
-    echo " -- Week: all --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-    /usr/bin/timew summary "${week_start}" - "${endtimestamp}" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo " -- Week: all --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    /usr/bin/timew summary "${week_start}" - "${ENDTIMESTAMP}" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
 
-    echo >> ${OUTPUTDIR}/timesheet-$enddate.html
-    echo >> ${OUTPUTDIR}/timesheet-$enddate.html
-    echo " -- Month: totals --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-    /usr/bin/timew report totals.py :month | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    echo >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    echo " -- Month: totals --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    /usr/bin/timew report totals.py :month | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
 
-    echo >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
     for p in "foss" "job" "research" "personal"
     do
-        echo " -- Month: $p --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-        /usr/bin/timew summary "${month_start}" - "${endtimestamp}" "$p" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+        echo " -- Month: $p --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+        /usr/bin/timew summary "${month_start}" - "${ENDTIMESTAMP}" "$p" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
     done
 
-    echo " -- Month: all --" >> ${OUTPUTDIR}/timesheet-$enddate.html
-    /usr/bin/timew summary "${month_start}" - "${endtimestamp}" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$enddate.html
+    echo " -- Month: all --" >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
+    /usr/bin/timew summary "${month_start}" - "${ENDTIMESTAMP}" | ansi2html -w >> ${OUTPUTDIR}/timesheet-$ENDDATE.html
 
 }
 
 usage () {
-    echo "make-task-report.sh [-t] [-h]"
+    echo "make-task-report.sh [-do] [-twh]"
     echo
     echo "Script generates a task and time sheet reports"
     echo
     echo "Options:"
     echo
     echo "-d <date>: date to use as end date/time for time sheets: if not supplied, use today's date and current time"
+    echo "-o <outputdir>: location to save generated files to"
     echo "-t: generate only task reports assuming today's date and current time"
     echo "-w: generate only timew reports assuming today's date and current time"
     echo "-h: print this help message and exit"
 }
 
 # parse options
-while getopts "twd:h" OPTION
+while getopts "twd:o:h" OPTION
 do
     case $OPTION in
         t)
-            enddate=${enddateinput:-$enddatedefault}
-            endtimestamp=${enddateinput:-$nowtime}
+            ENDDATE=${ENDDATEINPUT:-$ENDDATEDEFAULT}
+            ENDTIMESTAMP=${ENDDATEINPUT:-$NOWTIME}
+            OUTPUTDIR=${OUTPUTDIRINPUT:-$OUTPUTDIRDEFAULT}
             taskreports
             exit 0
             ;;
         w)
-            enddate=${enddateinput:-$enddatedefault}
-            endtimestamp=${enddateinput:-$nowtime}
+            ENDDATE=${ENDDATEINPUT:-$ENDDATEDEFAULT}
+            ENDTIMESTAMP=${ENDDATEINPUT:-$NOWTIME}
+            OUTPUTDIR=${OUTPUTDIRINPUT:-$OUTPUTDIRDEFAULT}
             timesheets
             exit 0
             ;;
+        o)
+            OUTPUTDIRINPUT="$OPTARG"
+            ;;
         d)
-            enddateinput="$OPTARG"
+            ENDDATEINPUT="$OPTARG"
             ;;
         h)
             usage
@@ -127,7 +133,8 @@ do
 done
 
 
-enddate=${enddateinput:-$enddatedefault}
-endtimestamp=${enddateinput:-$nowtime}
+ENDDATE=${ENDDATEINPUT:-$ENDDATEDEFAULT}
+ENDTIMESTAMP=${ENDDATEINPUT:-$NOWTIME}
+OUTPUTDIR=${OUTPUTDIRINPUT:-$OUTPUTDIRDEFAULT}
 taskreports
 timesheets
