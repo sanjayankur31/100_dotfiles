9aac3acbddcbb57eaab63cc363f5edb913710a84
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-01-13 20:47:28 +0000

    feat(task): do not update mtime before syncing
    Now that I'm backing up on all machines, this isn't right. I do need to
    remember to not run task until the db has synced though. No workaround
    for that.

--- bin/backup-taskw.sh
+++ bin/backup-taskw.sh
@@ -9,7 +9,7 @@ timestamp="$(date +%Y%m%d%H%M)"
 filename="taskchampion.sqlite3"
 
 # update mtime of the db for syncing
-echo "Pending tasks: $(task status:pending count)"
+# echo "Pending tasks: $(task status:pending count)"
 
 pushd ~/.task/ && cp -av  "${filename}" "${filename}.${timestamp}.$HOSTNAME" && popd
 find ~/.task/ -name "${filename}.*.$HOSTNAME" -mtime +28 -printf "removed '%f'\n " -delete
