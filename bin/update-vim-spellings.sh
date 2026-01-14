83da2d556650e76738335ba81efdfb14b0f994bf
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-01-14 11:16:54 +0000

    fix(vim): update spelling script

--- bin/update-vim-spellings.sh
+++ bin/update-vim-spellings.sh
@@ -6,12 +6,22 @@
 #
 # Removes any merge conflict markers, sorts the words, removes duplicates.
 
-SPELLFILE="en.utf-8.add"
+set -e
+
+OURLANG="en.utf-8"
+SPELLFILE="${OURLANG}.add"
 SPELLDIR="$HOME"/.vim/spell
 
 pushd "$SPELLDIR" || exit -1
-    cp ${SPELLFILE} ${SPELLFILE}.backup
-    sed -i -e '/^=+$/ d' -e '/^>+$/ d' -e '/^<+$/ d' "$SPELLFILE"
-    sort -h "$SPELLFILE" | uniq > "$SPELLFILE.new"
+    # make sure we have everything, including syncthing conflict files
+    sort -h "${OURLANG}*.add" "${SPELLFILE}*backup" | uniq > "${SPELLFILE}.new"
+
+    # no longer needed, not under git
+    # sed -i -e '/^=+$/ d' -e '/^>+$/ d' -e '/^<+$/ d' "$SPELLFILE"
+
+    cp "${SPELLFILE}.new" "${SPELLFILE}.backup" -v
     mv "$SPELLFILE.new" "$SPELLFILE" -v
+
+    # remove sync conflict files
+    rm *.sync-conflict* -fv
 popd
