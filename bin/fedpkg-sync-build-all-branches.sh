03d64ace31809e96ce1fb0667a103cd438b2a6ae
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-06-28 20:53:19 +0100

    feat(fedpkg-sync): allow impact checks outside git-scm folders

--- bin/fedpkg-sync-build-all-branches.sh
+++ bin/fedpkg-sync-build-all-branches.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
 # Copyright 2023 Ankur Sinha
-# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 # File : bin/fedpkg-sync-build-all-branches.sh
 #
 # Just a shortcut so I don't have to write the for loop each time myself.
 #
 #
 
-if ls *.spec > /dev/null 2>&1
-then
-    PACKAGE_NAME="$(basename *.spec .spec)"
-    echo "Working on package ${PACKAGE_NAME}"
-else
-    echo "Could not find a spec file in this folder. Exiting"
-    exit 1
-fi
-
-branches=""
+PACKAGE_NAME=""
+branches=()
 UPDATE_NOTE="meh"
 TYPE="bugfix"
 IMPACT_CHECK="No"
@@ -27,21 +19,21 @@ if ! command -v fedrq &> /dev/null
 then
     echo ">> fedrq not found. Please install fedrq:"
     echo ">> sudo dnf install fedrq"
-    exit -1
+    exit 1
 fi
 
 if ! command -v fedpkg &> /dev/null
 then
     echo ">> fedpkg not found! Exiting."
-    exit -1
+    exit 1
 fi
 
 impact_check () {
     echo ">> Checking update impact using fedrq for ${branch}"
     echo ">> The following packages will be affected. Please ensure that they do not break as a result of this update:"
-    ALL_PROVIDES="($(fedrq subpkgs -F provides ${PACKAGE_NAME} | cut -d  "=" -f1 | sed -e 's/\(lib.*.so\).*$/\1/' -e 's/\s*$//' -e 's/(/\\(/g' -e 's/)/\\)/g' | tr '\n' '|'  | sed 's/|$//'))"
+    ALL_PROVIDES="($(fedrq subpkgs -F provides "${PACKAGE_NAME}" | cut -d  "=" -f1 | sed -e 's/\(lib.*.so\).*$/\1/' -e 's/\s*$//' -e 's/(/\\(/g' -e 's/)/\\)/g' | tr '\n' '|'  | sed 's/|$//'))"
     # echo "${PACKAGE_NAME} provides: "${ALL_PROVIDES}
-    DEPS=( $(fedrq whatrequires-src -b "${branch}" -X "${PACKAGE_NAME}") )
+    mapfile -t DEPS < <(fedrq whatrequires-src -b "${branch}" -X "${PACKAGE_NAME}")
     for dep in "${DEPS[@]}"
     do
         echo "*** ${dep} ***"
@@ -51,18 +43,18 @@ impact_check () {
 }
 
 run () {
-    if [ "" == "${branches}" ]
+    if [ ${#branches[@]} -eq 0 ]
     then
         echo ">> No branches suplied. Exiting"
         usage
-        exit -1
+        exit 1
     fi
 
     echo ">> Updating repo and rebasing"
     git checkout rawhide
     git pull --rebase
 
-    for branch in $branches
+    for branch in "${branches[@]}"
     do
         echo ">> Working on branch: ${branch}"
 
@@ -84,38 +76,39 @@ then
         if  [ "meh" == "${UPDATE_NOTE}" ]
         then
             echo ">> Merging and building"
-            fedpkg switch-branch "$branch" && git pull && git merge rawhide && git push && fedpkg build || exit -1
+            fedpkg switch-branch "$branch" && git pull && git merge rawhide && git push && fedpkg build || exit 1
         else
             echo ">> Merging, building, creating update"
-            fedpkg switch-branch "$branch" && git pull && git merge rawhide && git push && fedpkg build && fedpkg update --type "${TYPE}" --notes "${UPDATE_NOTE}" || exit -1
+            fedpkg switch-branch "$branch" && git pull && git merge rawhide && git push && fedpkg build && fedpkg update --type "${TYPE}" --notes "${UPDATE_NOTE}" || exit 1
     fi
     done
 }
 
 usage () {
     echo "Usage: "
-    echo "$0 [-utI][-i] <branches to sync with rawhide>"
+    echo "$0 [-utIip] <branches to sync with rawhide>"
     echo
     echo "Options:"
     echo "-i only run impact check"
+    echo "-I skip impact check (because you've done this already)"
     echo "-u <update note>"
     echo "-t <update type>: see 'fedpkg update -h' for valid options"
-    echo "-I skip impact check (because you've done this already)"
+    echo "-p <package name>: specify package for impact check (when not in a package git folder)"
     echo
     echo "Positional parameters:"
-    echo "<branches to sync with rawhide>"
+    echo "<branches> branches to run impact check for, or sync from rawhide"
 }
 
 if [ $# -lt 1 ]
 then
     echo "At least one positional argument is required"
     usage
-    exit -1
+    exit 1
 fi
 
 # parse options
 # https://stackoverflow.com/questions/11742996/is-mixing-getopts-with-positional-parameters-possible
-while getopts "Iiu:t:h" OPTION
+while getopts "Iiu:t:p:h" OPTION
 do
     case $OPTION in
         u)
@@ -123,6 +116,9 @@ do
             ;;
         t)
             TYPE="$OPTARG"
+            ;;
+        p)
+            PACKAGE_NAME="$OPTARG"
             ;;
         i)
             IMPACT_CHECK="Yes"
@@ -134,14 +130,43 @@ do
             usage
             exit 0
             ;;
+        *)
+            usage
+            exit 0
+            ;;
     esac
 done
 
-shift $(($OPTIND - 1))
-branches="$@"
+shift $((OPTIND - 1))
+branches=("$@")
+
+if [ ${#branches[@]} -eq 0 ]
+then
+    echo ">> No branches supplied. Exiting"
+    usage
+    exit 1
+fi
+
+if [ "" == "${PACKAGE_NAME}" ]
+then
+    if ls ./*.spec > /dev/null 2>&1
+    then
+        PACKAGE_NAME="$(basename ./*.spec .spec)"
+        echo "Working on package ${PACKAGE_NAME}"
+    else
+        echo "Could not find a spec file in this folder."
+        echo "Use -p <package-name> to specify the package for impact checks."
+        exit 1
+    fi
+else
+    echo ">> Package name provided: ${PACKAGE_NAME}"
+    echo ">> Running in impact-check-only mode. To merge and build, run from a package git folder."
+    IMPACT_CHECK="Yes"
+fi
+
 if [ "Yes" == "${IMPACT_CHECK}" ]
 then
-    for branch in $branches
+    for branch in "${branches[@]}"
     do
         impact_check
     done
